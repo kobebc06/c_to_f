6678a1b57a5460a7d7f660c78312b4b6ae655bb5
Barry <kobebarry0629@gmail.com>
2024-07-26 17:16:14 -0700

    Add new file

---
+++ if_practice.py
@@ -0,0 +1,13 @@
+#age = input("please enter your age:")
+
+#if int(age) >= 20:
+	#print("eligible for voting")
+#else:
+	#print("ineligible")
+
+
+temp = input('please enter the temperature:')
+
+fahr = float(temp)*(9/5) + 32
+
+print("currennt temperature in Fahrenheit is:", fahr)
